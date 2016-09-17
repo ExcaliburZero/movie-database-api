96750a772a0b1fc1006d558fd010140768b57c3a
Christopher Wells <cwellsny@nycap.rr.com>
2016-09-17 15:37:34 -0400

    Document main module
    Add some documentation to the main module of the program.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,10 @@
+-- |
+-- Module      : Main
+-- Description : Implements an application for hosting a web api of a
+--               movie database.
+-- Copyright   : (c) Christopher Wells, 2016
+-- License     : MIT
+-- Maintainer  : cwellsny@nycap.rr.com
 module Main where
 
 import Console.Options
@@ -13,8 +20,10 @@ main = defaultMain $ do
       putStrLn $ unwords ["Serving database", show (toParam databaseFile)
                          , "on port", show defaultPort, "..."]
 
+-- | A parser for parsing the database file argument.
 databaseFileArgument :: OptionDesc r (Arg String)
 databaseFileArgument = argument "databaseFile" Right
 
+-- | The default port number used for the web api.
 defaultPort :: Int
 defaultPort = 8001
