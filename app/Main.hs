29978bbdf027255eb1ac9f05043e98eb2c191e37
Christopher Wells <cwellsny@nycap.rr.com>
2016-09-17 15:07:08 -0400

    Add basic cli functionality
    Add basic cli functionality to setup the program interface.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import Console.Options
+
 import Database.WebAPI.Movies
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = defaultMain $ do
+  programName "movie-database-api"
+  command "serve" $ do
+    databaseFile <- databaseFileArgument
+    action $ \toParam ->
+      putStrLn $ unwords ["Serving database", show (toParam databaseFile)
+                         , "on port", show defaultPort, "..."]
+
+databaseFileArgument :: OptionDesc r (Arg String)
+databaseFileArgument = argument "databaseFile" Right
+
+defaultPort :: Int
+defaultPort = 8001
