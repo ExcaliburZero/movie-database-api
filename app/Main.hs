dfccd83d6b9b6e2dc47aacc9b4791bc062853f7d
Christopher Wells <cwellsny@nycap.rr.com>
2016-10-04 20:52:15 -0400

    Improve cli functionality
    Add a port flag to specify the port to serve the api on. Also add more information on the cli interface.

--- app/Main.hs
+++ app/Main.hs
@@ -8,23 +8,39 @@
 module Main where
 
 import Console.Options
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import Data.Version (makeVersion)
+import Text.Read (readMaybe)
 
 import Database.WebAPI.Movies
 
 main :: IO ()
 main = defaultMain $ do
   programName "movie-database-api"
+  programDescription "An application for generating and serving a movie database."
+  programVersion $ makeVersion [0,1,0]
 
   command "serve" $ do
+    description "Serves the given database as a web api."
+
     databaseFile <- databaseFileArgument
+    portNumber <- flagParam
+      (FlagLong "port" <> FlagDescription "The port to serve the api on.")
+      (FlagRequired portNumberParser)
+
     action $ \toParam -> do
       let databasePath = toParam databaseFile
+      let port = fromMaybe defaultPort (toParam portNumber)
       putStrLn $ unwords ["Serving database", show databasePath
-                         , "on port", show defaultPort, "..."]
-      serveDatabase databasePath defaultPort
+                         , "on port", show port, "..."]
+      serveDatabase databasePath port
 
   command "create" $ do
+    description "Creates an empty database at the given filepath."
+
     databaseFile <- databaseFileArgument
+
     action $ \toParam -> do
       let databasePath = toParam databaseFile
       putStrLn $ "Creating database at " ++ show databasePath ++ "..."
@@ -38,3 +54,9 @@ databaseFileArgument = argument "databaseFile" Right
 -- | The default port number used for the web api.
 defaultPort :: Int
 defaultPort = 8001
+
+-- | Parser for the port number flag argument.
+portNumberParser :: String -> Either String Int
+portNumberParser x = case (readMaybe :: String -> Maybe Int) x of
+  Just n  -> Right n
+  Nothing -> Left $ "Invalid port number " ++ x
